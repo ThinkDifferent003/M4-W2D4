f81bf0d66a8b2c2b713b209b3e665210eab1a3c6
ThinkDifferent003 <rolandbibal3@gmail.com>
2026-01-12 19:28:30 +0100

    ex

--- M4W2D2/Assets/Scripts/MouseClick.cs
+++ M4W2D2/Assets/Scripts/MouseClick.cs
@@ -8,7 +8,10 @@ public class MouseClick : MonoBehaviour
     public float _force = 100f;
     public float _offset = 0.01f;
     
+    // Start is called before the first frame update
    
+
+    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -24,13 +27,40 @@ public class MouseClick : MonoBehaviour
                     Vector3 forceDirection = _ray.direction * _force;
                     _rb.AddForceAtPosition(forceDirection, hit.point);
                     Debug.Log("Forza Applicata");
+
+                    Vector3 point = hit.transform.InverseTransformPoint(hit.point);
+                    float margin = 0.45f;
+
+                    Vector3 clamp = new Vector3(
+                        Mathf.Clamp(point.x, -margin, margin),
+                        Mathf.Clamp(point.y, -margin, margin),
+                        Mathf.Clamp(point.z, -margin, margin)
+                        );
+                    Vector3 localNormal = hit.transform.InverseTransformDirection(hit.normal);
+                    if (localNormal.x != 0)
+                    {
+                        clamp.x = point.x;
+                    }
+                    if (localNormal.y != 0)
+                    {
+                        clamp.y = point.y;
+                    }
+                    if (localNormal.z != 0)
+                    {
+                        clamp.z = point.z;
+                    }
+
+                    
+
+                    Vector3 spawnPosition = hit.transform.TransformPoint(clamp) + (hit.normal * _offset);
+
+                    Quaternion spawnRotation = Quaternion.LookRotation(-hit.normal);
+
+                    GameObject hole = Instantiate(_bulletHolePrefab, spawnPosition, spawnRotation);
+                    hole.transform.SetParent(hit.transform);
+                    hole.transform.localScale = Vector3.one;
+                    hole.transform.localScale = Vector3.one * 0.1f;
                 }
-                Vector3 spawnPosition = hit.point + (hit.normal * _offset);
-
-                Quaternion spawnRotation = Quaternion.LookRotation(-hit.normal);
-
-                GameObject hole = Instantiate (_bulletHolePrefab, spawnPosition, spawnRotation);
-                hole.transform.SetParent(hit.transform);
             }
         }
     }
